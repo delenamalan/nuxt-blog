e3f64b0d4b55b4a9fedf9ec1fe21e670b84a47d8
Delena Malan <delenamalan@gmail.com>
2019-08-26 20:17:11 +0200

    Add navigation and first pages

---
+++ components/Navigation.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <ul class="flex">
+      <li v-for="page in navigation_pages" :key="page.name" class="-mb-px mr-1">
+        <nuxt-link
+          :to="{ name: page.name }"
+          class="outline-none focus:bg-gray-800 inline-block py-2 px-4 rounded-t text-white"
+          :class="isActive(page) ? active_classes : inactive_classes"
+          >{{ page.display }}</nuxt-link
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      active_classes: ['border-l', 'border-t', 'border-r'],
+      inactive_classes: ['bg-gray-700'],
+      navigation_pages: [
+        {
+          display: 'Usesthis',
+          name: 'usesthis',
+          active: true
+        },
+        {
+          display: 'Lovesthis',
+          name: 'lovesthis'
+        }
+      ]
+    }
+  },
+  methods: {
+    isActive(page) {
+      return this.$route.name === page.name
+    }
+  }
+}
+</script>
--- pages/usesthis.vue
+++ pages/usesthis.vue
@@ -1,8 +1,38 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <div>
+      <logo />
+      <h1 class="title">
+        Uses this
+      </h1>
+      <h2 class="subtitle">
+        My blog
+      </h2>
+      <div class="links">
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script></script>
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
 
 <style>
 /* Sample `apply` at-rules with Tailwind CSS
