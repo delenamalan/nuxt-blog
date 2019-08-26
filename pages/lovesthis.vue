6dcc9bea3ff116d2c9f4c709f66f054201d7309d
Delena Malan <delenamalan@gmail.com>
2019-08-26 23:34:10 +0200

    Add lovesthis content

--- pages/lovesthis.vue
+++ pages/lovesthis.vue
@@ -1,73 +1,197 @@
 <template>
-  <div class="container">
+  <div>
     <div>
-      <logo />
-      <h1 class="title">
-        Uses this
-      </h1>
-      <h2 class="subtitle">
-        My blog
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+      <div class="page-content">
+        <div>
+          <h3>Blogs</h3>
+          <ul class="list-dash">
+            <li><a href="https://news.ycombinator.com/">Hacker News</a>.</li>
+            <li>
+              <a href="http://www.aaronsw.com/weblog/">Raw Thought</a> by the
+              sadly deceased Aaron Swartz.
+            </li>
+            <li>
+              <a href="http://blog.ncase.me/">Nicky Case</a>, the expert in
+              explorable explanations.
+            </li>
+            <li>
+              <a href="https://bost.ocks.org/mike/">Mike Bostock</a>, the expert
+              in data visualisations and author of D3JS.
+            </li>
+            <li>
+              <a href="http://candychang.com/work/">Candy Chang</a> who creates
+              awesome public art projects.
+            </li>
+            <li>
+              <a href="http://lauren-mccarthy.com/">Lauren McCarthy</a> who
+              create weird public tech-art projects.
+            </li>
+            <li><a href="https://hackernoon.com/">HackerNoon</a>.</li>
+            <li>
+              <a href="http://stephaniehurlburt.com/blog-archive/"
+                >Stephanie Hulburt</a
+              >
+              written by a very successful and mindful female engineer and
+              entrepreneur.
+            </li>
+            <li>
+              <a href="https://www.justinobeirne.com/">Justin O’Beirne</a>, the
+              Google Maps expert.
+            </li>
+            <li>
+              <a href="https://zachholman.com/">https://zachholman.com/</a>.
+            </li>
+            <li><a href="https://blog.codinghorror.com/">Coding Horror</a>.</li>
+          </ul>
+        </div>
+        <div>
+          <h2 id="podcasts">Podcasts</h2>
+          <ul class="list-dash">
+            <li>
+              <a href="https://www.wnyc.org/shows/notetoself">Note to Self</a>.
+            </li>
+            <li>
+              <a href="http://99percentinvisible.org/">99 Percent Invisible</a>,
+              if you only ever try one podcast, try this one!
+            </li>
+            <li>
+              <a href="https://www.bloomberg.com/podcasts/decrypted"
+                >Decrypted</a
+              >.
+            </li>
+            <li>
+              <a href="http://gretchenrubin.com/podcast/"
+                >Happier by Gretchen Rubin</a
+              >.
+            </li>
+            <li>
+              <a href="http://www.storycentral.org/strangers/">Strangers</a>.
+            </li>
+            <li>
+              <a href="https://www.radiolab.org/">RadioLab</a>, weird and
+              wonderful stories that will fascinate you. Some of my favourite
+              episodes include:
+              <a href="http://www.radiolab.org/story/ceremony/">The Ceremony</a>
+              about the launch of a new cryptocurrency and
+              <a href="http://www.radiolab.org/story/gondo/">The Gondolier</a>
+              about the antifeminist transman who was the first woman to become
+              a gondolier in Venice.
+            </li>
+            <li>
+              <a href="https://www.npr.org/podcasts/510308/hidden-brain"
+                >Hidden Brain</a
+              >, a podcast that investigate the unconscious patterns that drive
+              us as humans.
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h2 id="books">Books</h2>
+          <h3 id="non-fiction">Non-fiction</h3>
+          <ul class="list-dash">
+            <li>
+              <a
+                href="https://www.goodreads.com/book/show/26114505-age-of-discovery"
+                >Age of Discovery: Navigating the Risks and Rewards of Our New
+                Renaissance</a
+              >, a comparison between our current age and that of the
+              Renaissance period.
+            </li>
+            <li>
+              <a
+                href="https://www.goodreads.com/book/show/13152194-tiny-beautiful-things"
+                >Tiny Beautiful Things: Advice on Love and Life from Dear
+                Sugar</a
+              >, the most beautiful, heart-felt letters which are sure to make
+              you a more compassionate and understanding human being.
+            </li>
+            <li>
+              <a href="https://www.goodreads.com/book/show/12262741-wild"
+                >Wild: From Lost to Found on the Pacific Crest Trail by Cheryl
+                Strayed</a
+              >, by the same author from Tiny Beautiful Things: a journey of
+              self-discovery on one of the longest hiking trails in the world.
+            </li>
+            <li>
+              <a
+                href="https://www.goodreads.com/book/show/28186015-weapons-of-math-destruction"
+                >Weapons of Math Destruction: How Big Data Increases Inequality
+                and Threatens Democracy</a
+              >, a book to remind us as technologists to know what the impact of
+              our technology could be on other humans.
+            </li>
+            <li>
+              <a
+                href="https://www.goodreads.com/book/show/13588356-daring-greatly"
+                >Daring Greatly by Brené Brown</a
+              >.
+            </li>
+            <li>
+              <a
+                href="https://www.goodreads.com/book/show/29093292-the-daily-stoic"
+                >The Daily Stoic: 366 Meditations for Clarity, Effectiveness,
+                and Serenity</a
+              >, because we all need to be reminded to stay calm sometimes.
+            </li>
+            <li>
+              <a href="https://www.goodreads.com/book/show/7507825-at-home"
+                >At Home: A Short History of Private Life</a
+              >, a life-time’s worth of small-talk fun facts and insight into
+              the captivating history of some of the most mundane objects we use
+              daily.
+            </li>
+            <li>
+              <a
+                href="https://www.goodreads.com/book/show/6516450-what-the-dog-saw-and-other-adventures"
+                >What the Dog Saw and Other Adventures by Malcolm Gladwell</a
+              >.
+            </li>
+            <li>
+              <a
+                href="https://www.goodreads.com/book/show/6346975-moonwalking-with-einstein"
+                >Moonwalking with Einstein: The Art and Science of Remembering
+                Everything by Joshua Foer</a
+              >, a book on the limits of our memory.
+            </li>
+            <li>
+              <a href="https://www.goodreads.com/book/show/22543496-elon-musk"
+                >Elon Musk: Inventing the Future by Ashlee Vance</a
+              >.
+            </li>
+            <li>
+              <a
+                href="https://www.goodreads.com/book/show/18296055-what-to-talk-about"
+                >What to Talk About: On a Plane, at a Cocktail Party, in a Tiny
+                Elevator with Your Boss’s Boss by Chris Colin, Robert
+                Baedeker</a
+              >, the official guide to making small-talk.
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h3 id="fiction">Fiction</h3>
+          <ul class="list-dash">
+            <li>
+              <a
+                href="https://www.goodreads.com/book/show/24997390-a-strangeness-in-my-mind"
+                >A Strangeness in My Mind by Orhan Pamuk</a
+              >.
+            </li>
+            <li>
+              <a
+                href="https://www.goodreads.com/book/show/19288239-colorless-tsukuru-tazaki-and-his-years-of-pilgrimage"
+                >Colorless Tsukuru Tazaki and His Years of Pilgrimage by Haruki
+                Murakami</a
+              >.
+            </li>
+            <li>
+              <a href="https://www.goodreads.com/book/show/38990.Fun_Home"
+                >Fun Home: A Family Tragicomic by Alison Bechdel</a
+              >.
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
 </template>
-
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
